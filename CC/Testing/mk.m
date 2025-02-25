2017d4228cf88fa56e013cf8d3e994bd2ad28782
tony maggs <acmaggs@gmail.com>
2025-02-25 11:57:09 +0100

    Remove Cholesky testing

--- CC/Testing/mk.m
+++
@@ -1,6 +0,0 @@
-function M=mk(N)
-%periodic laplacian operator, with pinning on one site, to remove zero ev
-M = diag(2*ones(1,N)) + diag(-1*ones(1,N-1),1) + diag(-1*ones(1,N-1),-1);
-M(N,N)=10;
-M(1,N)=-1;
-M(N,1)=-1;

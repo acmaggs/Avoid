7a675ecc79d6f4185cb38cfd1c7cdbd36d90d19a
tony maggs <acmaggs@gmail.com>
2025-11-10 16:35:40 +0100

    format

--- CC/avoid.cc
+++ CC/avoid.cc
@@ -13,263 +13,263 @@
 using namespace std;
 // energy (x_i - x_{i+1})^2/2 event drive mc on gaussian chain, to compare with true self-avoiding walk
 
-namespace Param{
-  int N = 128; //number of particles
-  int iter = 1024*128; //number of samples
-  const int cold=true;  //hot or cold start
-  const double T=1; // temperature
-  const int GRAPH=1;//animation on screen
-}
+namespace Param {
+int N = 128;           // number of particles
+int iter = 1024 * 128; // number of samples
+const int cold = true; // hot or cold start
+const double T = 1;    // temperature
+const int GRAPH = 1;   // animation on screen
+} // namespace Param
 
 Graphics *g;
 
-static string reset="\u001b[0m";
-static string yellow="\u001b[33m";
-static string red="\u001b[31m";
+static string reset = "\u001b[0m";
+static string yellow = "\u001b[33m";
+static string red = "\u001b[31m";
 
-mt19937_64 gen(random_device{}()); //seed random number from quality source
-uniform_real_distribution<double> uniform(0.,1.);
-normal_distribution<double> gauss(0.0,1.0);
+mt19937_64 gen(random_device{}()); // seed random number from quality source
+uniform_real_distribution<double> uniform(0., 1.);
+normal_distribution<double> gauss(0.0, 1.0);
 
-
-void
-build_cholesky(vector<double> &d, vector<double> &o, vector<double> &c){ //cholesky factor for 1d laplacian
-  int n= d.size();
-  for(int i=0;i<n-1 ;i++) {
-     d[i] = sqrt( (i+2.)/(i+1.));//diagonal
-     o[i] = -1/d[i]; // diagonal+1
+void build_cholesky(vector<double> &d, vector<double> &o, vector<double> &c) { // cholesky factor for 1d laplacian
+  int n = d.size();
+  for (int i = 0; i < n - 1; i++) {
+    d[i] = sqrt((i + 2.) / (i + 1.)); // diagonal
+    o[i] = -1 / d[i];                 // diagonal+1
   }
-  d[n-1]= sqrt(8.);//arbitrary to have +ve matrix
-  for(int i=0;i<n;i++) c[i] = -1./sqrt( (i+1)*(i+2)); //correction for periodic bc
-  c[n-2]= -sqrt( n/(n-1.));
-  c[n-1]=d[n-1];
-  o[n-2]= c[n-2];
+  d[n - 1] = sqrt(8.); // arbitrary to have +ve matrix
+  for (int i = 0; i < n; i++)
+    c[i] = -1. / sqrt((i + 1) * (i + 2)); // correction for periodic bc
+  c[n - 2] = -sqrt(n / (n - 1.));
+  c[n - 1] = d[n - 1];
+  o[n - 2] = c[n - 2];
 }
 
-void
-random_config(vector<double> &conf, vector<double> & d,vector<double> & o,vector<double> & c, int test){ //solve for a configuration by back substitution
-  int n=d.size();
+void random_config(vector<double> &conf, vector<double> &d, vector<double> &o, vector<double> &c,
+                   int test) { // solve for a configuration by back substitution
+  int n = d.size();
   vector<double> r(n);
-  if(!test) {
-    for (int i=0;i<n;i++) r[i]=gauss(gen);
-  }
-  else{
-    for (int i=0;i<n;i++) r[i]=sqrt(i+1);
+  if (!test) {
+    for (int i = 0; i < n; i++)
+      r[i] = gauss(gen);
+  } else {
+    for (int i = 0; i < n; i++)
+      r[i] = sqrt(i + 1);
   }
 
-  conf[n-1]= r[n-1]/d[n-1];
-  conf[n-2]= (r[n-2]-conf[n-1]*o[n-2] )/ d[n-2];
-  for(int i=0;i<n-2;i++) conf[n-3-i]= (r[n-3-i]-conf[n-2-i]*o[n-3-i] - c[n-3-i]*conf[n-1])/ d[n-3-i];
+  conf[n - 1] = r[n - 1] / d[n - 1];
+  conf[n - 2] = (r[n - 2] - conf[n - 1] * o[n - 2]) / d[n - 2];
+  for (int i = 0; i < n - 2; i++)
+    conf[n - 3 - i] = (r[n - 3 - i] - conf[n - 2 - i] * o[n - 3 - i] - c[n - 3 - i] * conf[n - 1]) / d[n - 3 - i];
 }
 
-
-void code(){//print the code to this program
+void code() { // print the code to this program
 #include "avoid.hex"
-  for(auto i=0U;i<avoid_cc_len;i++)
-    cout<<avoid_cc[i];
+  for (auto i = 0U; i < avoid_cc_len; i++)
+    cout << avoid_cc[i];
   exit(0);
 }
 
-double
-step(vector<double> & x, vector<int> & count, int & a, int &b){//single ECMC step
-  int n= x.size();
+double step(vector<double> &x, vector<int> &count, int &a, int &b) { // single ECMC step
+  int n = x.size();
   double r_step, l_step, energy;
-  int right = (a+1) % n;
-  int left= (a-1+n) % n;
-  
-  //collision to the right
-  double sep=x[right]-x[a];
-  energy =  -Param::T*log( uniform(gen));
-  if ( sep < 0 )
-    energy += sep*sep/2.;
-  r_step= sqrt(2*energy)+sep;
+  int right = (a + 1) % n;
+  int left = (a - 1 + n) % n;
 
-  //collision to the left
-  sep=x[a] - x[left];
-  energy = -Param::T*log( uniform(gen));
-  if( sep > 0 )
-    energy += sep*sep/2.;
-  l_step =  sqrt( energy*2) -sep;
+  // collision to the right
+  double sep = x[right] - x[a];
+  energy = -Param::T * log(uniform(gen));
+  if (sep < 0)
+    energy += sep * sep / 2.;
+  r_step = sqrt(2 * energy) + sep;
 
-  
-  if (r_step<l_step){ //now choose between two candidates
+  // collision to the left
+  sep = x[a] - x[left];
+  energy = -Param::T * log(uniform(gen));
+  if (sep > 0)
+    energy += sep * sep / 2.;
+  l_step = sqrt(energy * 2) - sep;
+
+  if (r_step < l_step) { // now choose between two candidates
     x[a] += r_step;
-    a=right;
+    a = right;
     b++;
-  }
-  else{
+  } else {
     x[a] += l_step;
-    a=left;
+    a = left;
     b--;
   }
   count[a]++;
   return min(r_step, l_step);
 }
 
-void
-sweep(vector<double> & x, vector<int> &count, int & a, int &b){ //N ECMC steps
-  for (int i=0; i<Param::N;i++){
+void sweep(vector<double> &x, vector<int> &count, int &a, int &b) { // N ECMC steps
+  for (int i = 0; i < Param::N; i++) {
     step(x, count, a, b);
-  }  
+  }
   count[a]--;
 }
 
-void
-build_harmonic(vector<double> & x){
-  int n=x.size();
-  vector<double> d(n),o(n), c(n);
+void build_harmonic(vector<double> &x) {
+  int n = x.size();
+  vector<double> d(n), o(n), c(n);
   build_cholesky(d, o, c);
-  random_config(x, d,o, c,0);
+  random_config(x, d, o, c, 0);
 }
 
-void
-save(vector<double> const&  x, vector<int> const & master, list<int> const & lst, list<int> const & blst, list<int> const & zlst, list<double> const & xlst){
-  ofstream cnt ("count.dat"); //average L as a function of position
-  ofstream opos ("pos.dat"); //final configuration
-  ofstream lout ("list.dat"); //H for rho_2
-  ofstream bout ("blist.dat");// unwrapped position of active pointer, for rho_1
-  ofstream zout ("zlist.dat"); //returns to origin
-  ofstream xout ("xlist.dat"); //x[a] for last point in simulation
-  
-  for (double i : x) opos<<i<<"\n";
-  for (int i : master) cnt<<i<<"\n";
-  for (int n : lst) lout << n << "\n";
-  for (int n : blst) bout << n << "\n";
-  for (int n : zlst) zout << n << "\n";
-  for (double xx : xlst) xout << xx <<"\n";
+void save(vector<double> const &x, vector<int> const &master, list<int> const &lst, list<int> const &blst, list<int> const &zlst,
+          list<double> const &xlst) {
+  ofstream cnt("count.dat");  // average L as a function of position
+  ofstream opos("pos.dat");   // final configuration
+  ofstream lout("list.dat");  // H for rho_2
+  ofstream bout("blist.dat"); // unwrapped position of active pointer, for rho_1
+  ofstream zout("zlist.dat"); // returns to origin
+  ofstream xout("xlist.dat"); // x[a] for last point in simulation
+
+  for (double i : x)
+    opos << i << "\n";
+  for (int i : master)
+    cnt << i << "\n";
+  for (int n : lst)
+    lout << n << "\n";
+  for (int n : blst)
+    bout << n << "\n";
+  for (int n : zlst)
+    zout << n << "\n";
+  for (double xx : xlst)
+    xout << xx << "\n";
 }
 
-void
-start(vector<double> & x){
-  int a=0;
-  int b=0;
-  vector<int> count(Param::N); 
-  for (int i=0; i<Param::N ;i++){ //equilibration steps
-    sweep( x , count, a, b);
-  }
-  cout<<"Warm start"<<endl;
-}
-
-void 
-chain(vector<double> &x, vector<int> & count, int & a, int & b){
-  double t_chain=0;
-  double sample_time= Param::N;
-  if(Param::GRAPH) sample_time *= 100;
-  int olda,oldb;
-  while(true){
-    olda=a;
-    oldb=b;
-    double s=step(x,count,a,b);
-    if( t_chain+s>sample_time ){// we have gone slightly too far, step back
-        a=olda;
-        b=oldb;
-        count[olda]--;
-        x[olda] -=  (-sample_time+ t_chain +s);
-        return;
-    }
-    else{
-        t_chain += s;
-    }
-   if(Param::GRAPH)
-        g->draw(x,  60 , a , a);
-  }
-}
-
-void
-mc(vector<double> &  x, vector<int>  & master, list<int> & lst, list<int>  & blst, list<int>  & zlst, list<double>  & xlst, ofstream & log_out){
-  uniform_int_distribution<> ran_int(0,Param::N-1); // distribution in range [0, N-1]
+void start(vector<double> &x) {
+  int a = 0;
+  int b = 0;
   vector<int> count(Param::N);
- if(Param::GRAPH) {
-    double dmin=0.02*Param::N;
-    double dmax=Param::N*1.05;
-    double diameter=0.2;
-    g= new Graphics(Param::N, 1800, dmin, dmax, diameter);
+  for (int i = 0; i < Param::N; i++) { // equilibration steps
+    sweep(x, count, a, b);
   }
-  for (int l=0; l<Param::iter; l++){ //main loop for data 
-    int a;
-    if( l % (1024*8) == 0) {
-      cout<<"l "<<l/1024<<endl;
-      log_out<<"l "<<l/1024<<" of "<<Param::iter/1024<<endl<<flush;
+  cout << "Warm start" << endl;
+}
+
+void chain(vector<double> &x, vector<int> &count, int &a, int &b) {
+  double t_chain = 0;
+  double sample_time = Param::N;
+  if (Param::GRAPH)
+    sample_time *= 100;
+  int olda, oldb;
+  while (true) {
+    olda = a;
+    oldb = b;
+    double s = step(x, count, a, b);
+    if (t_chain + s > sample_time) { // we have gone slightly too far, step back
+      a = olda;
+      b = oldb;
+      count[olda]--;
+      x[olda] -= (-sample_time + t_chain + s);
+      return;
+    } else {
+      t_chain += s;
     }
-    if( l % (1024*128) == 0)   save(x, master, lst, blst, zlst, xlst);
-    
+    if (Param::GRAPH)
+      g->draw(x, 60, a, a);
+  }
+}
+
+void mc(vector<double> &x, vector<int> &master, list<int> &lst, list<int> &blst, list<int> &zlst, list<double> &xlst, ofstream &log_out) {
+  uniform_int_distribution<> ran_int(0, Param::N - 1); // distribution in range [0, N-1]
+  vector<int> count(Param::N);
+  if (Param::GRAPH) {
+    double dmin = 0.02 * Param::N;
+    double dmax = Param::N * 1.05;
+    double diameter = 0.2;
+    g = new Graphics(Param::N, 1800, dmin, dmax, diameter);
+  }
+  for (int l = 0; l < Param::iter; l++) { // main loop for data
+    int a;
+    if (l % (1024 * 8) == 0) {
+      cout << "l " << l / 1024 << endl;
+      log_out << "l " << l / 1024 << " of " << Param::iter / 1024 << endl << flush;
+    }
+    if (l % (1024 * 128) == 0)
+      save(x, master, lst, blst, zlst, xlst);
+
     if (Param::cold) {
       std::fill(x.begin(), x.end(), 0); // reset position
       if (Param::GRAPH)
-          for (int i=0;i<Param::N;i++) x[i] = 60*cos( (2.*i*M_PI)/Param::N)-20;
-    }
-    else{
+        for (int i = 0; i < Param::N; i++)
+          x[i] = 60 * cos((2. * i * M_PI) / Param::N) - 20;
+    } else {
       build_harmonic(x);
     }
 
     a = 0;
-    int a0 = a; //remember origin
-    int b = 0; // "b" is almost "a" but unwrapped to follow activity
-    std::fill(count.begin(), count.end(), 0); // reset count of site visits    
-    chain( x , count, a, b); //chain() or replace by sweep()
-    lst.push_back( count[a] ); // H curve, rho_2
-    blst.push_back( b ); //displacement, rho_1
-    xlst.push_back( x[a] ); 
-    zlst.push_back( count[a0] ); //origin visits
-    
-    for(int i=0; i<Param::N; i++){//global count curve, not analysed
-      int pos = (a0+i)%Param::N;
+    int a0 = a;                               // remember origin
+    int b = 0;                                // "b" is almost "a" but unwrapped to follow activity
+    std::fill(count.begin(), count.end(), 0); // reset count of site visits
+    chain(x, count, a, b);                    // chain() or replace by sweep()
+    lst.push_back(count[a]);                  // H curve, rho_2
+    blst.push_back(b);                        // displacement, rho_1
+    xlst.push_back(x[a]);
+    zlst.push_back(count[a0]); // origin visits
+
+    for (int i = 0; i < Param::N; i++) { // global count curve, not analysed
+      int pos = (a0 + i) % Param::N;
       master[i] += count[pos];
     }
   }
 }
 
-void
-test() {
-  cout<<"Test of cholesky code, to compare with matlab solution for debugging"<<endl;
+void test() {
+  cout << "Test of cholesky code, to compare with matlab solution for debugging" << endl;
   vector<double> x(7);
-  int n=x.size();
-  vector<double> d(n),o(n), c(n);
+  int n = x.size();
+  vector<double> d(n), o(n), c(n);
   build_cholesky(d, o, c);
-  random_config(x, d,o, c,1);
-  cout<<setprecision(5);
-  cout<<"d \t";
-  for(auto y:d) cout<<y<<" ";
-  cout<<"\no \t";
-  for(auto y:o) cout<<y<<" ";
-  cout<<"\nc \t";
-  for(auto y:c) cout<<y<<" ";
-  cout<<"\nx \t";
-  for(auto y:x) cout<<y<<" ";
-  cout<<endl;
-  cout<<"this last line should be"<<endl;
-  cout<<"  3.9533    5.5570    6.1357    5.8691    4.8665    3.2032    0.9354"<<endl;
+  random_config(x, d, o, c, 1);
+  cout << setprecision(5);
+  cout << "d \t";
+  for (auto y : d)
+    cout << y << " ";
+  cout << "\no \t";
+  for (auto y : o)
+    cout << y << " ";
+  cout << "\nc \t";
+  for (auto y : c)
+    cout << y << " ";
+  cout << "\nx \t";
+  for (auto y : x)
+    cout << y << " ";
+  cout << endl;
+  cout << "this last line should be" << endl;
+  cout << "  3.9533    5.5570    6.1357    5.8691    4.8665    3.2032    0.9354" << endl;
 }
 
-
-int
-main(int argc, char * argv[]){
-  if( argc ==2 && argv[1] == string("code"))//print out this c++ code
+int main(int argc, char *argv[]) {
+  if (argc == 2 && argv[1] == string("code")) // print out this c++ code
     code();
-  if (argc  > 1) 
+  if (argc > 1)
     Param::N = stoi(argv[1]);
   if (argc > 2)
-    Param::iter = 1024*stoi(argv[2]);
+    Param::iter = 1024 * stoi(argv[2]);
   test();
-  ofstream log_out ("harmonic.log");
-  log_out<<"git commit string "<<GIT_COMMIT<<endl;
-  list<int> zlst; // returns to origin, local time
-  list<int> lst; //measures h, rho_2
-  list<int> blst; //unwrapped final index, rho_1
-  list<double> xlst; //final position in x of chain
- 
+  ofstream log_out("harmonic.log");
+  log_out << "git commit string " << GIT_COMMIT << endl;
+  list<int> zlst;    // returns to origin, local time
+  list<int> lst;     // measures h, rho_2
+  list<int> blst;    // unwrapped final index, rho_1
+  list<double> xlst; // final position in x of chain
+
   vector<double> x(Param::N);
   vector<int> master(Param::N);
   Timer t1;
-  
-  cout<<yellow<<"N= "<<Param::N<<"Iterations="<<Param::iter<<"\tcold="<<Param::cold<<endl<<flush;
-  log_out<<"N="<<Param::N<<"\tIterations="<<Param::iter<<"\tcold="<<Param::cold<<endl<<flush;
+
+  cout << yellow << "N= " << Param::N << "Iterations=" << Param::iter << "\tcold=" << Param::cold << endl << flush;
+  log_out << "N=" << Param::N << "\tIterations=" << Param::iter << "\tcold=" << Param::cold << endl << flush;
 
   mc(x, master, lst, blst, zlst, xlst, log_out);
 
   save(x, master, lst, blst, zlst, xlst);
-  log_out<<"Done"<<endl;
-  cout<<red<<"Done"<<reset<<endl;
+  log_out << "Done" << endl;
+  cout << red << "Done" << reset << endl;
   t1.stats(log_out);
   return 0;
 }
--- CC/lattice.cc
+++ CC/lattice.cc
@@ -4,125 +4,133 @@
 #include <cassert>
 #include <fstream>
 #include <list>
-#include <algorithm>    // std::shuffle
+#include <algorithm> // std::shuffle
 #include "timer.h"
 #include "graphics.h"
 #include "version.h"
 
-namespace Param{
-  const int p=2; // lattice is p*Np sites
-  int  Np=256; //number of particles
-  const double p0=1-1./p; //probability
-  int miter=1024*128*4; //number of samples
-  const int do_equilibrate=0;  //hot or cold start
-  const int GRAPH=0;//animation on screen
-}
+namespace Param {
+const int p = 2;              // lattice is p*Np sites
+int Np = 256;                 // number of particles
+const double p0 = 1 - 1. / p; // probability
+int miter = 1024 * 128 * 4;   // number of samples
+const int do_equilibrate = 0; // hot or cold start
+const int GRAPH = 0;          // animation on screen
+} // namespace Param
 
 using namespace std;
 
-static string reset="\u001b[0m";
-static string yellow="\u001b[33m";
-static string red="\u001b[31m";
+static string reset = "\u001b[0m";
+static string yellow = "\u001b[33m";
+static string red = "\u001b[31m";
 
 Graphics *g;
 
-mt19937_64 gen(random_device{}()); //seed random numbers from high quality source
-uniform_real_distribution<double> uniform(0.,1.);
+mt19937_64 gen(random_device{}()); // seed random numbers from high quality source
+uniform_real_distribution<double> uniform(0., 1.);
 
-void code(){//print the code to this program
+void code() { // print the code to this program
 #include "lattice.hex"
-  for(auto i=0U;i<lattice_cc_len;i++)
-    cout<<lattice_cc[i];
+  for (auto i = 0U; i < lattice_cc_len; i++)
+    cout << lattice_cc[i];
   exit(0);
 }
 
-inline void 
-step(vector<int> & x, vector<int> & count, int & a, int &b){//single ECMC step
-  int n=x.size();
-  int L=Param::p*n;
-  int right = (a+1) % n;
-  int left = (a-1+n) % n; //a, right, left are indexes
+inline void step(vector<int> &x, vector<int> &count, int &a, int &b) { // single ECMC step
+  // "x[i]" is the position of the particle "i".
+  // "n=x.size()" is the number of particles
+  // "p*n" is the number of sites in the periodic system
+  // "a" is the pointer position, or the number of the active particle x[a] is it's position at the start of the routine.
+  // "b" is just "a" but without the periodic bc, does not need to be followed to understand logic
+  // in all my runs p0=1/2
+  // eq number correspond to arxiv: https://arxiv.org/pdf/2306.13059
+  int n = x.size();
+  int L = Param::p * n;
+  int right = (a + 1) % n;
+  int left = (a - 1 + n) % n; // a, right, left are indexes
 
-  if( (x[a] + 1) % L != x[right] ) {
-    x[a] =  (x[a]+1) % L; // jump right same particle
-    if( uniform(gen) < Param::p0 ){ //empty, 9b, alpha'
-    }
-    else{ // transfer motion to left 9b alpha
+  if ((x[a] + 1) % L != x[right]) { // there is no particle immediately to the right of the particle "a"
+    x[a] = (x[a] + 1) % L;          // jump right
+    if (uniform(gen) < Param::p0) { // empty, 9b, alpha'
+    } else {                        // transfer motion to left 9b alpha
       b--;
-      a=left;
+      a = left;
     }
-  }
-  else{ //full
-    if( uniform(gen) < Param::p0 ){ //transfer active particle, no motion 10b alpha'
-      a=right;
+  } else {                          // site to the right is full
+    if (uniform(gen) < Param::p0) { // transfer active particle, no motion 10b alpha'
+      a = right;
       b++;
-    }
-    else{//keep same particle
-      //10b alpha
+    } else { // keep same particle
+      // 10b alpha
     }
   }
   count[a]++;
 }
 
-void
-sweep(vector<int> & x, vector<int> &count, int & a, int &b){ //N ECMC steps
-  int factor=1;
-  if (Param::GRAPH) 
-	factor=100;
-  for ( auto i=0U; i<factor*x.size();i++){// note 1/2 here, prevents wrap-around in smaller systems
+void sweep(vector<int> &x, vector<int> &count, int &a, int &b) { // N ECMC steps
+  int factor = 1;
+  if (Param::GRAPH)
+    factor = 100;
+  for (auto i = 0U; i < factor * x.size(); i++) { // note 1/2 here, prevents wrap-around in smaller systems
     step(x, count, a, b);
-    if(Param::GRAPH)  
-	g->draw(x,  60 , i , a); 
+    if (Param::GRAPH)
+      g->draw(x, 60, i, a);
   }
 }
 
-void
-save(vector<int> const&  x,  list<int> const & lst, list<int> const & blst, list<int> & sep, vector<int> & count){
-  ofstream opos ("pos.dat");
-  ofstream lout ("list.dat");
-  ofstream bout ("blist.dat");
-  ofstream sout ("sep.dat");
-  ofstream ctout ("count.dat");
-  
-  for (double i : x) opos<<i<<"\n";
-  for (int n : lst) lout << n << "\n";
-  for (int n : blst) bout << n << "\n";
-  for (int n : sep) sout << n << "\n";
-  for (int n : count) ctout << n << "\n";
+void save(vector<int> const &x, list<int> const &lst, list<int> const &blst, list<int> &sep, vector<int> &count) {
+  ofstream opos("pos.dat");
+  ofstream lout("list.dat");
+  ofstream bout("blist.dat");
+  ofstream sout("sep.dat");
+  ofstream ctout("count.dat");
+
+  for (double i : x)
+    opos << i << "\n";
+  for (int n : lst)
+    lout << n << "\n";
+  for (int n : blst)
+    bout << n << "\n";
+  for (int n : sep)
+    sout << n << "\n";
+  for (int n : count)
+    ctout << n << "\n";
 }
 
-void sf(vector<int> &x){
-  vector<int> trial(Param::Np*Param::p);
-  for(auto j=0;j< Param::Np*Param::p; j++) trial[j] = j%Param::p == 0 ? 1 : 0;
-  shuffle ( trial.begin(), trial.end(), gen); //random permutation of trial
-  int found=0;
-  for(auto i=0;i< Param::p*Param::Np;i++ ){
-    if(trial[i] ==1){
-      x[found]=i;
+void sf(vector<int> &x) {
+  vector<int> trial(Param::Np * Param::p);
+  for (auto j = 0; j < Param::Np * Param::p; j++)
+    trial[j] = j % Param::p == 0 ? 1 : 0;
+  shuffle(trial.begin(), trial.end(), gen); // random permutation of trial
+  int found = 0;
+  for (auto i = 0; i < Param::p * Param::Np; i++) {
+    if (trial[i] == 1) {
+      x[found] = i;
       found++;
     }
   }
-  assert(found==Param::Np);
+  assert(found == Param::Np);
 }
 
-int
-main(int argc, char * argv[]){
-  if( argc ==2 && argv[1] == string("code"))
+int main(int argc, char *argv[]) {
+  if (argc == 2 && argv[1] == string("code"))
     code();
-  if (argc  > 1)
+  if (argc > 1)
     Param::Np = stoi(argv[1]);
   if (argc > 2)
-    Param::miter = 1024*stoi(argv[2]);
-  ofstream log_out ("tasep.log");
-  log_out<<GIT_COMMIT<<endl;
+    Param::miter = 1024 * stoi(argv[2]);
+  ofstream log_out("tasep.log");
+  log_out << GIT_COMMIT << endl;
   Timer t1;
-  cout<<yellow<<"Param::p="<<Param::p<<"\tNp="<<Param::Np<<"\tmiter="<<Param::miter<<"\tdo_equilibrate="<<Param::do_equilibrate<<reset<<endl;
-  log_out << "Param::p="<<Param::p<<"\tNp="<<Param::Np<<"\tmiter="<<Param::miter<<"\tdo_equilibrate="<<Param::do_equilibrate<<endl;
-  if(Param::GRAPH) {
-    double dmin=0.02*Param::p*Param::Np;
-    double dmax=Param::p*Param::Np*1.05;
-    double diameter=0.2;
-    g= new Graphics(Param::Np, 1400, dmin, dmax, diameter);
+  cout << yellow << "Param::p=" << Param::p << "\tNp=" << Param::Np << "\tmiter=" << Param::miter
+       << "\tdo_equilibrate=" << Param::do_equilibrate << reset << endl;
+  log_out << "Param::p=" << Param::p << "\tNp=" << Param::Np << "\tmiter=" << Param::miter << "\tdo_equilibrate=" << Param::do_equilibrate
+          << endl;
+  if (Param::GRAPH) {
+    double dmin = 0.02 * Param::p * Param::Np;
+    double dmax = Param::p * Param::Np * 1.05;
+    double diameter = 0.2;
+    g = new Graphics(Param::Np, 1400, dmin, dmax, diameter);
   }
   vector<int> x(Param::Np);
   vector<int> count(Param::Np);
@@ -130,27 +138,29 @@ main(int argc, char * argv[]){
   list<int> blst;
   list<int> lst;
   list<int> sep;
-  
-  for (int i=0;i<Param::miter;i++){ //main loop over samples
-    if( i % (8*1024) == 0) {
-      cout<<"l "<<i/1024<<endl;
-      log_out<<"l "<<i/1024<<" of "<<Param::miter/1024 <<endl<<flush;
+
+  for (int i = 0; i < Param::miter; i++) { // main loop over samples
+    if (i % (8 * 1024) == 0) {
+      cout << "l " << i / 1024 << endl;
+      log_out << "l " << i / 1024 << " of " << Param::miter / 1024 << endl << flush;
     }
-    int b=0;//b is a, but unwrapped to follow displacement of activity
-    int a=0;
-    for(auto j=0;j< Param::Np; j++) 	x[j] = Param::p*j; //restart
+    int b = 0; // b is a, but unwrapped to follow displacement of activity
+    int a = 0;
+    for (auto j = 0; j < Param::Np; j++)
+      x[j] = Param::p * j; // restart
     if (Param::do_equilibrate)
       sf(x);
     std::fill(count.begin(), count.end(), 0); // reset count of site visits
-    sweep(x, count, a,  b); 
-    blst.push_back( b ); //displacement
-    lst.push_back( count[a] ); //for H
-    for(auto m=0U; m< count.size(); m++) total[m] += count[m];// total site visits
+    sweep(x, count, a, b);
+    blst.push_back(b);       // displacement
+    lst.push_back(count[a]); // for H
+    for (auto m = 0U; m < count.size(); m++)
+      total[m] += count[m]; // total site visits
   }
-  
-  save( x,  lst,  blst, sep, total);
-  cout<<red<<"Done"<<reset<<endl;
-  log_out<<"Done"<<endl;
+
+  save(x, lst, blst, sep, total);
+  cout << red << "Done" << reset << endl;
+  log_out << "Done" << endl;
   t1.stats(log_out);
   return 0;
 }
